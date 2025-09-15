7d15930011fbc114b30e27a91d6933ff26313203
JK-17 <118494774+JK-17@users.noreply.github.com>
2025-09-15 14:58:06 +0900

    Time: 1 ms (58.38%), Space: 19 MB (33.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Goal => timecomplexity O(n)
+        # use dictionary
         hashMap = {}
+        # use enumerate to find index(i), and value (num) in array (nums)
         for i, num in enumerate(nums):
+            # subtract to find correspoding value
             diff = target - num
+            # if there is in the dictionary (means you know where(index) and what(value))
             if diff in hashMap:
+                # return it with the value
                 return [hashMap[diff], i]
+            # else (there is no corresponding value in dictionary)
+            # append them in dictionary (for next compare or to find the answer)
             hashMap[num] = i
